ce8c89b42c9ea86bf15fb0d7ecdc9bfa5e5b2dc7
Vladimir <themyvlink@gmail.com>
2019-04-04 12:59:48 +0500

    создал репозиторий

---
+++ src/components/Home.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+        <v-container fluid>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-carousel>
+                        <v-carousel-item
+                            v-for="ad in ads"
+                            :key="ad.id"
+                            :src="ad.imageSrc"
+                        >
+                            <div class="car-link">
+                                <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+                            </div>
+                        </v-carousel-item>
+                    </v-carousel>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+        <v-container grid-list-lg>
+            <v-layout row wrap>
+                <v-flex
+                    xs12
+                    sm6
+                    md4
+                    v-for="ad of ads"
+                    :key="ad.id"
+                >
+                    <v-card>
+                        <v-img
+                            :src="ad.imageSrc"
+                            aspect-ratio="2.75"
+                        ></v-img>
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-0">{{ ad.title }}</h3>
+                                <div> {{ ad.description }} </div>
+                            </div>
+                        </v-card-title>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
+                            <v-btn raised class="primary">Buy</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            ads: [
+                {
+                    title: 'First ad',
+                    description: 'Hello i am description',
+                    promo: false,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+                    id: '1'
+                },
+                {
+                    title: 'Second ad',
+                    description: 'Hello i am description',
+                    promo: true,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+                    id: '2'
+                },
+                {
+                    title: 'Third ad',
+                    description: 'Hello i am description',
+                    promo: true,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+                    id: '3'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .car-link {
+        position: absolute;
+        bottom: 50px;
+        left: 50%;
+        background: rgba(0, 0, 0, 0.5);
+        transform: translateX(-50%);
+        padding: 5px 15px;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 5px;
+    }
+</style>
+
---
+++ src/components/User/Orders.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list two-line subheader>
+          <v-list-tile
+            
+            v-for="order in orders"
+            :key="order.id"
+          >
+            <v-list-tile-action>
+              <v-checkbox
+                color="success"
+                :input-value="order.done"
+                @change="markDone(order)"
+              ></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content @click="notifications = !notifications">
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+                :to="'/ad/' + order.adId"
+                class="primary"
+              >Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      orders: [
+        {
+          id: 'fds3',
+          name: 'Vova',
+          phone: '8-800-3000-600',
+          adId: '1',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
+    }
+  }
+}
+</script>
